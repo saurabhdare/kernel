6567dd7bc8beacf4948197749060040e8bb0f5e6
Saurabh Dhandare <saurabhdhandare@gmail.com>
2024-04-20 19:37:25 +0530

    Implemented printf

--- kernel.cpp
+++ kernel.cpp
@@ -1,3 +1,11 @@
+void printf(char* str)
+{
+    unsigned short * VideoMemory = (unsigned short*) 0xb8000;
+    for (int i = 0;  str[i] != '\0'; ++i) {
+        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+    }
+}
+
 void kernelMain(void *multiboot_structure, unsigned int magicnumber)
 {
     printf("Hello, world!");
